2dac0f24646cd3fe8bf206415d7850c1c2756e35
SimonasMaz <simonas.mazylis@gmail.com>
2022-10-27 23:19:37 +0300

    task 3 not fully completed, minimum style,no responsive

--- Task 3/script.js
+++ Task 3/script.js
@@ -11,4 +11,30 @@ Pastaba: Sukurta kortelė, kurioje yra pateikiama vartotojo informacija, turi
 turėti bent minimalų stilių ir būti responsive;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'https://api.github.com/users';
+const ENDPOINT = "https://api.github.com/users";
+
+document.getElementById("btn").addEventListener("click", () => {
+  const users = async () => {
+    try {
+      const response = await fetch(ENDPOINT);
+      const data = await response.json();
+      pushResults(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  users();
+});
+
+const pushResults = element => {
+  element.forEach(item => {
+    const avatar = document.createElement("img");
+    avatar.src = item.avatar_url;
+    avatar.style.width = "150px";
+
+    const login = document.createElement("h3");
+    login.textContent = item.login;
+
+    output.append(avatar, login);
+  });
+};
