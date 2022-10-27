67ddf275b5d9c8061c6687559e870c2d26fafabf
SimonasMaz <simonas.mazylis@gmail.com>
2022-10-27 22:57:57 +0300

    Task 1 completed

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,32 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+
+const output = document.getElementById("output");
+const target = document.getElementById("search");
+
+document.getElementById("submit-btn").addEventListener("click", e => {
+  e.preventDefault();
+  const kgValue = target.value;
+
+  if (kgValue) {
+    const lb = (kgValue * 2.2046).toFixed(2);
+    const g = (kgValue / 0.001).toFixed(2);
+    const oz = (kgValue * 35.274).toFixed(2);
+    output.innerText =
+      "Pounds = " +
+      lb +
+      "lb || " +
+      "Grams = " +
+      g +
+      "g || " +
+      "Ounces = " +
+      oz +
+      "oz";
+    output.style.textAlign = "center";
+    output.style.margin = "50px";
+    output.style.fontSize = "1.1em";
+  } else {
+    output.innerText = "";
+  }
+});
