081458b425d2188e6d83167cfbd9520b48241b78
SimonasMaz <simonas.mazylis@gmail.com>
2022-10-27 23:48:44 +0300

    tast 4 not fully completed, minimum style,no responsive

--- Task 4/script.js
+++ Task 4/script.js
@@ -9,4 +9,34 @@ Pastaba: Sukurta kortelė, kurioje yra informacija apie automobilį (brand), tur
 turėti bent minimalų stilių ir būti responsive;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'cars.json';
+const ENDPOINT = "cars.json";
+
+const users = async () => {
+  try {
+    const response = await fetch("./cars.json");
+    const data = await response.json();
+    displayResults(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+users();
+function displayResults(value) {
+  value.forEach(item => {
+    const card = document.createElement("div");
+
+    card.classList.add("card");
+
+    const brand = document.createElement("h2");
+
+    brand.textContent = item.brand;
+
+    const models = document.createElement("p");
+
+    models.textContent = item.models;
+
+    card.append(brand, models);
+
+    output.append(card);
+  });
+}
